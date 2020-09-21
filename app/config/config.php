7ae20bf844cce4e3e78de8ef06bdc6ff6a3f7ace
KevinManimau <kevin03manimau@gmail.com>
2020-09-22 05:30:02 +0800

    congigurasi database

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('BASEURL', 'https://siao-pintu-air.herokuapp.com/public/');
 
 //DB
-define('DB_HOST','ec2-3-228-114-251.compute-1.amazonaws.com:5432');
+define('DB_HOST','ec2-3-228-114-251.compute-1.amazonaws.com');
 define('DB_USER','ymfdshjdoeyyzd');
 define('DB_PASS','a0c044bbce799d417cac4c0c65f73e96adbce3663e6b8c20775a459c0559a4a1');
 define('DB_NAME','dde7riplgel59i');
