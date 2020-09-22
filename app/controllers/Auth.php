37031f53638678b25a6f728fe2f7233d82ec03ab
KevinManimau <kevin03manimau@gmail.com>
2020-09-23 04:47:36 +0800

    change user to users table

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -12,50 +12,50 @@ class Auth extends Controller{
     {
         if($this->model('User_model')->getUserforAuth($_POST)){
             $usession = $this->model('User_model')->getUserforAuth($_POST);
-            $idrole = $usession['id_role'];
-            $myrole = $this->model('Role_model')->getRolebyId($idrole);
-            $menurole = $this->model('HakAkses_model')->getHakAksesbyIdRole($idrole);
-            $yourmenu=[];
-            $I=0;
-            foreach($menurole as $menu){
-                $menn = $this->model('Menu_model')->getMenubyId($menu['id_menu']);
-                $yourmenu[$I]=[
-                    "id_menu"=> $menn['id_menu'],
-                    "judul_menu"=>$menn['judul_menu'],
-                    "icon"=>$menn['icon'],
-                    "url"=>$menn['url'],
-                    "is_active"=>$menn['is_active']
-                ];
-                $I++;
-            }
+            // $idrole = $usession['id_role'];
+            // $myrole = $this->model('Role_model')->getRolebyId($idrole);
+            // $menurole = $this->model('HakAkses_model')->getHakAksesbyIdRole($idrole);
+            // $yourmenu=[];
+            // $I=0;
+            // foreach($menurole as $menu){
+            //     $menn = $this->model('Menu_model')->getMenubyId($menu['id_menu']);
+            //     $yourmenu[$I]=[
+            //         "id_menu"=> $menn['id_menu'],
+            //         "judul_menu"=>$menn['judul_menu'],
+            //         "icon"=>$menn['icon'],
+            //         "url"=>$menn['url'],
+            //         "is_active"=>$menn['is_active']
+            //     ];
+            //     $I++;
+            // }
 
-            $uid = $usession['id_user'];
-            // $timeuser = $this->model('User_model')->UpdateTimeUser($uid);
-            if($this->model('User_model')->UpdateTimeUser($uid) > 0){
-                $uid = $usession['id_user'];
-                $mgr =  $this->model('Auth_model')->getidManager($uid);
-                // $idmgr = $mgr['id_manager'];
-                $_SESSION['info'] = [
-                    'uname' => $usession['username'],
-                    'pass' => $_POST['password'], 
-                    'idrole' => $myrole['id'],
-                    'role' => $myrole['role']
-                ];
-                $_SESSION['manager'] = $mgr;
-                $_SESSION['menu']=$yourmenu;
-                header('Location: ' . BASEURL . 'Home');
+            // $uid = $usession['id_user'];
+            // // $timeuser = $this->model('User_model')->UpdateTimeUser($uid);
+            // if($this->model('User_model')->UpdateTimeUser($uid) > 0){
+            //     $uid = $usession['id_user'];
+            //     $mgr =  $this->model('Auth_model')->getidManager($uid);
+            //     // $idmgr = $mgr['id_manager'];
+            //     $_SESSION['info'] = [
+            //         'uname' => $usession['username'],
+            //         'pass' => $_POST['password'], 
+            //         'idrole' => $myrole['id'],
+            //         'role' => $myrole['role']
+            //     ];
+            //     $_SESSION['manager'] = $mgr;
+            //     $_SESSION['menu']=$yourmenu;
+            //     header('Location: ' . BASEURL . 'Home');
 
-            }else{
-                Flasher::setFlash('Gagal','username dan password anda salah','danger','icon-close');
-                header('Location: ' . BASEURL . 'Auth');
-                exit;
-            }       
+            // }else{
+            //     Flasher::setFlash('Gagal','username dan password anda salah','danger','icon-close');
+            //     header('Location: ' . BASEURL . 'Auth');
+            //     exit;
+            // }       
+            echo $usession;
         }else{
             Flasher::setFlash('Gagal','username dan password anda salah','danger','icon-close');
             header('Location: ' . BASEURL . 'Auth');
             exit;
         }
-        echo $usession;
         
 
 
